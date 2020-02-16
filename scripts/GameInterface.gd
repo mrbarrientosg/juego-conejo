7ff267e908ccec94279053c1799c4219b5d13a09
Matias Barrientos <matias.barrientosg@gmail.com>
2020-02-16 14:03:19 -0300

    alpha

--- scripts/GameInterface.gd
+++ scripts/GameInterface.gd
@@ -2,36 +2,21 @@ extends Control
 
 onready var scene_tree: SceneTree = get_tree()
 onready var pause_overlay: ColorRect = $PauseOverlay
+onready var score: Label = $Label
 
 var paused: bool = false setget set_paused
-var minimum_drag: float = 100.0
-var swipe_start: Vector2
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventScreenDrag:
-		print("0")
+func _ready() -> void:
+	PlayerData.connect("score_updated", self, "update_interface")
+	update_interface()
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("pause"):
-		swipe_start = get_global_mouse_position()
-		scene_tree.set_input_as_handled()
-		print(swipe_start)
-	if event.is_action_released("pause"):
-		_calculate_swipe(get_global_mouse_position())
-
-
-func _calculate_swipe(swipe_end: Vector2) -> void:
-	if swipe_start == null: 
-		return
-	var swipe = swipe_end - swipe_start
-	print(swipe)
-	if abs(swipe.y) > minimum_drag:
-		if swipe.y > 0:
-			print("1")
-		else:
-			print("2")
+func update_interface() -> void:
+	score.text = "Score: %s" % PlayerData.score
 
 func set_paused(value: bool) -> void:
 	paused = value
-	scene_tree.paused = value
 	pause_overlay.visible = value
+	scene_tree.paused = value
+
+func _on_Pause_pressed() -> void:
+	self.paused = not paused
--- scripts/RetryButton.gd
+++ scripts/RetryButton.gd
@@ -5,3 +5,4 @@ func _on_button_up() -> void:
 	PlayerData.reset()
 	get_tree().paused = false
 	get_tree().reload_current_scene()
+	
